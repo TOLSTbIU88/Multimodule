21d618caa21523c16bd136b7d78c5cf969e6ea26
konstantinvolkov <gamer-9600@yandex.ru>
2022-10-12 10:09:53 +0300

    phone class

--- Service/src/main/java/module_3/Phone.java
+++ Service/src/main/java/module_3/Phone.java
@@ -2,14 +2,16 @@ package module_3;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.ToString;
 
 @Builder
 @Getter
 @ToString
 public class Phone {
-
+    @NonNull
     private String prefix;
+    @NonNull
     private String number;
     private String bonusCode;
 }
