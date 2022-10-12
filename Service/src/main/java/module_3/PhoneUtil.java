91cbea050f8948ce84fd5241f31ff9a76aa0bdf6
konstantinvolkov <gamer-9600@yandex.ru>
2022-10-12 14:34:24 +0300

    phone class

--- Service/src/main/java/module_3/PhoneUtil.java
+++ Service/src/main/java/module_3/PhoneUtil.java
@@ -1,4 +1,11 @@
 package module_3;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class PhoneUtil {
+
+    public String getFullNumber(Phone phone) {
+        return phone.getPrefix()+phone.getNumber();
+    }
 }
